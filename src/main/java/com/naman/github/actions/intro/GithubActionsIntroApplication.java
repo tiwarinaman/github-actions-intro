5f04593d58d3394a5df832f8978ea2e09b1c8400
Naman Kumar <namant744@gmail.com>
2023-05-12 00:59:03 +0530

    initial commit

---
+++ src/main/java/com/naman/github/actions/intro/GithubActionsIntroApplication.java
@@ -0,0 +1,13 @@
+package com.naman.github.actions.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naman/github/actions/intro/GithubActionsIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naman.github.actions.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
